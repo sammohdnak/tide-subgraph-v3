c71a6d49059054148af2a16ce9565db975b79749
mendesfabio <ms.fabiomendes@gmail.com>
2024-01-24 08:35:25 -0300

    create and loader for pool tokens

--- subgraphs/vault/src/helpers/entities.ts
+++ subgraphs/vault/src/helpers/entities.ts
@@ -1,6 +1,7 @@
-import { Address } from "@graphprotocol/graph-ts";
-import { Pool, PoolSnapshot } from "../types/schema";
+import { Address, BigInt } from "@graphprotocol/graph-ts";
+import { Pool, PoolSnapshot, PoolToken } from "../types/schema";
 import { PoolShare } from "../types/schema";
+import { ZERO_BI } from "./constants";
 
 const DAY = 24 * 60 * 60;
 
@@ -18,16 +19,43 @@ export function createPoolSnapshot(pool: Pool, timestamp: i32): void {
     let poolAddress = pool.id;
     let dayTimestamp = timestamp - (timestamp % DAY);
     
-    let snapshotId = poolAddress + '-' + dayTimestamp.toString();
+    let snapshotId = poolAddress.toHex() + '-' + dayTimestamp.toString();
     let snapshot = PoolSnapshot.load(snapshotId);
   
     if (!snapshot) {
       snapshot = new PoolSnapshot(snapshotId);
     }
+
+    let poolTokens = pool.tokens.load();
+    let balances = new Array<BigInt>(poolTokens.length);
+    for (let i = 0; i < poolTokens.length; i++) {
+        balances[i] = poolTokens[i].balance;
+    }
   
     snapshot.pool = poolAddress;
-    snapshot.balances = pool.balances;
-    snapshot.totalShares = pool.totalShares;
+    snapshot.balances = balances;
     snapshot.timestamp = dayTimestamp;
+    snapshot.totalShares = pool.totalShares;
     snapshot.save();
+}
+
+export function createPoolToken(poolAddress: Address, tokenAddress: Address): void {
+    let poolTokenId = poolAddress.concat(tokenAddress);
+    let poolToken = PoolToken.load(poolTokenId);
+
+    if (!poolToken) {
+        poolToken = new PoolToken(poolTokenId);
+    }
+
+    poolToken.pool = poolAddress;
+    poolToken.address = tokenAddress;
+    poolToken.balance = ZERO_BI;
+    poolToken.save();
+}
+
+export function loadPoolToken(poolAddress: Address, tokenAddress: Address): PoolToken {
+    let poolTokenId = poolAddress.concat(tokenAddress);
+    let poolToken = PoolToken.load(poolTokenId) as PoolToken;
+
+    return poolToken;
 }
